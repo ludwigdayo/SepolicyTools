fbf50ef84726a56f9a031108416fcd10817bfaba
succerseng <succerseng@gmail.com>
2022-04-29 19:59:25 +0800

    feat:sepolicy tools

---
+++ src/FormatSepolicy.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+/**
+ * 优化selinux政策
+ */
+public class FormatSepolicy {
+    public static void main(String[] args) throws IOException, InterruptedException {
+//        FormatFileContexts formatFileContexts = new FormatFileContexts("sepolicy/file_contexts");
+//        formatFileContexts.formatFileContexts();
+//
+//        FormatFilesUtils.formatFiles("sepolicy", formatFileContexts);
+    }
+}
